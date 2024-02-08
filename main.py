24063fd37705bbd867cf9a06ed84ba7d929297db
Awwab <mawwabkhank2006@gmail.com>
2024-02-08 12:35:29 +0500

    post request to make graph

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import requests
 
 PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
+TOKEN = 'aweioklngaopijsdfkj'
+USERNAME = 'awwab'
 
 user_params = {
-    "token": "aweioklngaopijsdfkj",
-    "username": 'awwab',
+    "token": TOKEN,
+    "username": USERNAME,
     'agreeTermsOfService': 'yes',
     'notMinor': 'yes',
 }
 
 # response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
 # print(response.text)
+
+GRAPH_ENDPOINTS = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+graph_params = {
+    'id':'graph1',
+    'name': 'Coding Graph',
+    'unit': 'hr',
+    'type': 'float',
+    'color': 'momiji'
+}
+
+graph_headers = {
+    'X-USER-TOKEN': TOKEN
+}
+
+response = requests.post(url=GRAPH_ENDPOINTS, json=graph_params, headers=graph_headers)
+print(response.text)
