7240731731217ad8c1537ce7753fc0477f5daea8
Awwab <mawwabkhank2006@gmail.com>
2024-02-08 12:20:51 +0500

    setup pixela endpoint and parameters'

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
+
+user_params = {
+    "token": "aweioklngaopijsdfkj",
+    "username": 'awwab',
+    'agreeTermsOfService': 'yes',
+    'notMinor': 'yes',
+}
+
